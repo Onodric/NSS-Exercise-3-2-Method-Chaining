1e0c14e444f09649e3a7c4515469c33beede0777
Ben Marks <onodric@yahoo.com>
2016-10-19 11:30:28 -0500

    fixed the sort

--- js/main.js
+++ js/main.js
@@ -7,4 +7,13 @@
 
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
-document.getElementById("outputHere").innerHTML = integers.sort(function(a,b){return a-b}).filter(function(a){return a<20}).map(function(a){return a*1.5-1}).reduce(function(preValue, curValue, index, array) {return preValue + curValue;});
+var newIntegers = integers.sort(function(a,b){return b-a})
+console.log("new Array: ", newIntegers);
+newIntegers = integers.sort(function(a,b){return b-a}).filter(function(a){return a<20});
+console.log("new Array: ", newIntegers);
+newIntegers = integers.sort(function(a,b){return b-a}).filter(function(a){return a<20}).map(function(a){return a*1.5-1});
+console.log("new Array: ", newIntegers);
+newIntegers = integers.sort(function(a,b){return b-a}).filter(function(a){return a<20}).map(function(a){return a*1.5-1}).reduce(function(preValue, curValue) {return preValue + curValue;});
+console.log("new Array: ", newIntegers);
+
+document.getElementById("outputHere").innerHTML = integers.sort(function(a,b){return b-a}).filter(function(a){return a<20}).map(function(a){return a*1.5-1}).reduce(function(preValue, curValue){return preValue + curValue});
